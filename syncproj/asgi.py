6a1bad9ca90d264b314acb03e077852567b2287d
Grace Lu <Grace@MacBook-Air-103.local>
2025-11-14 14:58:27 -0600

    Initial Django project setup

---
+++ syncproj/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for syncproj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syncproj.settings')
+
+application = get_asgi_application()
